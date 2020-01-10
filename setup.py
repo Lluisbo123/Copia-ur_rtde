4edc3bc6c7946ffa189a23ed627613a90af3d26c
Anders Prier Lindvig <anpl@mmmi.sdu.dk>
2020-01-10 09:49:26 +0100

    Bumped version number to 0.0.5

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="ur_rtde",
-    version="0.0.4",
+    version="0.0.5",
     author="Anders Prier Lindvig",
     author_email="anpl@mmmi.sdu.dk",
     description="Python interface for sending and receiving data to/from a UR robot using the Real-Time Data Exchange (RTDE) interface of the robot",
