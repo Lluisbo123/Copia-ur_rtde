fb9fee748901d1ff1bc5502aeba4104f474737fe
Anders Prier Lindvig <anpl@mmmi.sdu.dk>
2021-08-03 19:19:46 +0000

    Update power_and_brake_release.py

--- ci/power_and_brake_release.py
+++ ci/power_and_brake_release.py
@@ -3,6 +3,6 @@ import dashboard_client
 import time 
 
 time.sleep(1)
-db_client = dashboard_client.DashboardClient("192.168.56.101", 29999, True)
+db_client = dashboard_client.DashboardClient("localhost", 29999, True)
 db_client.connect()
 db_client.brakeRelease()
