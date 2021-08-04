353fe0f668c22da9b91a5a3ca33a4789cde54b17
Anders Prier Lindvig <anpl@mmmi.sdu.dk>
2021-08-04 15:14:27 +0200

    Added initial test of interface against simulated controller in CI Pipeline

--- ci/power-and-brake-release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-python3 "$SCRIPTPATH/power_and_brake_release.py"
--- ci/power_and_brake_release.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import dashboard_client 
-import time 
-
-time.sleep(1)
-db_client = dashboard_client.DashboardClient("192.168.56.101", 29999, True)
-db_client.connect()
-db_client.brakeRelease()
